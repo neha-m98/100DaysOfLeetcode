505a5c64aa2820ea2c0c2670a9536b6bf9b87afc
neha-m98 <66993152+neha-m98@users.noreply.github.com>
2023-03-30 11:54:30 +0530

    Time: 4 ms (59.24%), Space: 42.5 MB (54.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -10,9 +10,11 @@ class Solution {
         Stack<Integer> numStack=new Stack<>();
         
         //create the map and add the length
+        //key is the first parameter and the value is the second in map
         numbersMap.put(nums2[nums2.length-1],-1);
         numStack.push(nums2[nums2.length-1]);
         
+        //start from 2nd last bcoz for last the element the next graeter is already found
         for(int i=nums2.length-2;i>=0;i--){
             if(nums2[i]<numStack.peek()){
                 numbersMap.put(nums2[i],numStack.peek());
@@ -20,13 +22,18 @@ class Solution {
                 continue;
             }
             
+            
+            //after popping
             while(!numStack.isEmpty() && numStack.peek()<nums2[i])
                 numStack.pop();
             
+            // if it is empty, then check if empty do 
             if(numStack.isEmpty()){
               numbersMap.put(nums2[i],-1);
               numStack.push(nums2[i]);  
             }
+            
+            //else add the the element
             else {
              numbersMap.put(nums2[i], numStack.peek());
              numStack.push(nums2[i]);
@@ -37,7 +44,7 @@ class Solution {
              for (int i = 0; i < nums1.length; i++)
              nums1[i] = numbersMap.get(nums1[i]);
 
-    return nums1;
+            return nums1;
 
     }
 }
